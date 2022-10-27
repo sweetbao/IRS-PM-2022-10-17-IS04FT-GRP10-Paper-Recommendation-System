d43bf135a786eddcb2b9ec1d2e9ed9cff719740d
duwenlei <750811582@qq.com>
2022-10-27 16:20:33 +0800

    return keywords

--- backend/PaperRecommend/views.py
+++ backend/PaperRecommend/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
@@ -14,10 +16,9 @@ class PaperViewSet(viewsets.ModelViewSet):
      queryset = Paper.objects.all().order_by('-retrievetime')
      serializer_class = PaperSerializer
      def get_queryset(self):
-        title = self.request.query_params.get("title", None)
-        if title:
-            qs = Paper.objects.filter()
-            qs = qs.filter(title=title)
+        domain = self.request.query_params.get("domain", None)
+        if domain:
+            qs = randomKeywords(domain)
 
             return qs
 
@@ -29,7 +30,7 @@ def addData(request):
     #storeData()
     #summaryGet()
     #keywordsGet()
-    randomKeywords('PaperRecommend')
+    randomKeywords('stat.ML')
 
 
     return HttpResponse('添加成功')
