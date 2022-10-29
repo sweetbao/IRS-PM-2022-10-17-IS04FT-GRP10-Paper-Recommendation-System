4cda1f1d5531d7e6cda92f0b76248fd282fb7d50
Daipp <554700142@qq.com>
2022-10-29 10:54:55 +0800

    Final integrate with backend

--- frontend/src/views/PaperView.vue
+++ frontend/src/views/PaperView.vue
@@ -20,6 +20,9 @@ export default {
   methods: {
     GetURL() {
       window.scrollTo(0, 0);
+    },
+    redirect(link){
+      window.location.href=link;
     }
   },
 
@@ -34,7 +37,7 @@ export default {
 
     const Getpaperlist = () => {
       axios.post(base_url + "testId/", state.SelectPapers).then(res => {
-        conslog.log(res);
+    
         state.Paper_list = res.data;
       }).catch(err => {
         console.log(err)
@@ -86,35 +89,88 @@ export default {
 .stt:active {
   opacity: 1;
 }
+
+figure { 
+  position: absolute;
+  margin: auto;
+  top: 0; bottom: 0; left: 0; right: 0;
+  width: 6.250em; height: 6.250em;
+  animation: rotate 2.4s linear infinite;
+}
+.white { 
+  top: 0; bottom: 0; left: 0; right: 0; 
+  background: white; 
+  animation: flash 2.4s linear infinite;
+  opacity: 0;
+}
+.dot {
+  position: absolute;
+  margin: auto;
+  width: 2.4em; height: 2.4em;
+  border-radius: 100%;
+  transition: all 1s ease;
+}
+.dot:nth-child(2) { top: 0; bottom: 0; left: 0; background: #FF4444; animation: dotsY 2.4s linear infinite; }
+.dot:nth-child(3) { left: 0; right: 0; top: 0; background: #FFBB33; animation: dotsX 2.4s linear infinite; }
+.dot:nth-child(4) { top: 0; bottom: 0; right: 0; background: #99CC00; animation: dotsY 2.4s linear infinite; }
+.dot:nth-child(5) { left: 0; right: 0; bottom: 0; background: #33B5E5; animation: dotsX 2.4s linear infinite; }
+
+@keyframes rotate {
+  0% { transform: rotate( 0 ); }
+  10% { width: 6.250em; height: 6.250em; }
+  66% { width: 2.4em; height: 2.4em; }
+  100%{ transform: rotate(360deg); width: 6.250em; height: 6.250em; }
+}
+
+@keyframes dotsY {
+  66% { opacity: .1; width: 2.4em; }
+  77%{ opacity: 1; width: 0; }
+}
+@keyframes dotsX {
+  66% { opacity: .1; height: 2.4em;}
+  77%{ opacity: 1; height: 0; }
+}
+
+@keyframes flash {
+  33% { opacity: 0; border-radius: 0%; }
+  55%{ opacity: .6; border-radius: 100%; }
+  66%{ opacity: 0; }
+}
 </style>
 <template>
+  <div v-if="Paper_list.length==0" style="margin-top:25%;margin-left: 25%;">
+<figure>
+  <div class="dot white"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+</figure>
+</div>
 
-  <div>{{ SelectPapers }}</div>
-
-  <div class="content" id="top">
-
-
+  <div class="content" id="top" >
+<div>
     <ol id="breathe-horizontal" start="1">
-      <li v-for="item in Paper_list" :key="item.url" class="arxiv-result">
+      <li v-for="item in Paper_list" :key="item.fields.url" class="arxiv-result">
         <div class="is-marginless">
-          <p class="list-title is-inline-block"><a :helf="item.link">{{ item.title }}</a></p>
+          <p class="list-title is-inline-block"><a :helf="item.fields.link" class="link external" @click="redirect(item.fields.link)" >{{ item.fields.title }}</a></p>
           <p class="authors"> <span class="has-text-black-bis has-text-weight-semibold">Authors: </span>
-            <label>{{ item.author }}</label>
+            <label>{{ item.fields.author }}</label>
           </p>
           <p class="abstract mathjax">
             <span class="search-hit">Abstract: </span>
-            <span class="abstract-short has-text-grey-dark mathjax" style="display: inline;">{{ item.abstract }}
+            <span class="abstract-short has-text-grey-dark mathjax" style="display: inline;">{{ item.fields.abstract }}
             </span>
           </p>
 
           <p class="is-size-7"><span class="has-text-black-bis has-text-weight-semibold">Submitted </span>
-            {{ item.publishTime }}
+            {{ item.fields.publishTime }}
 
 
           <p class="comments is-size-7">
 
             <span class="has-text-black-bis has-text-weight-semibold">Comments: </span>
-            <span class="has-text-grey-dark mathjax">{{ item.comment }}</span>
+            <span class="has-text-grey-dark mathjax">{{ item.fields.comment }}</span>
           </p>
 
           </p>
@@ -126,7 +182,7 @@ export default {
 
     <a @click="GetURL()" class="stt" title="Back to Top"></a>
   </div>
-
+</div>
 
 </template>
 
